af58ab7b0d3bd156e9e616e336cb43eee129b856
gr421_tikro <kristina.tibeykina04@mail.ru>
2023-12-22 12:59:22 +0700

    МОДЕЛЬ СОЗДАлд

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, blank=False, null=False)
+    description = models.TextField(max_length=1000, blank=False, null=False)
+    types = (('Товар', 'Товар'), ('Услуга', 'Услуга'))
+    type = models.CharField(choices=types, max_length=6)
+    image = models.ImageField(upload_to='media/')
+
